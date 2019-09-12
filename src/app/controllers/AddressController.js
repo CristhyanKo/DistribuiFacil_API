6b340c6774f096f82517de276bf818f48088cca6
Cristhyan <cristhyan.ko@outlook.com>
2019-09-12 14:44:56 -0400

    Validations in Address and Phone

--- src/app/controllers/AddressController.js
+++ src/app/controllers/AddressController.js
@@ -2,6 +2,13 @@ const Address = require("../models/Address");
 
 class AddressController {
 	async store(req, res) {
+		const { personId, main } = req.body;
+		if (await Address.findOne({ personId, main })) {
+			return res.status(400).json({
+				error: "There is already a registered address as main"
+			});
+		}
+
 		return res.json(await this.create(req.body));
 	}
 
--- src/app/controllers/PhoneController.js
+++ src/app/controllers/PhoneController.js
@@ -2,6 +2,13 @@ const Phone = require("../models/Phone");
 
 class PhoneController {
 	async store(req, res) {
+		const { personId, main } = req.body;
+		if (await Phone.findOne({ personId, main })) {
+			return res.status(400).json({
+				error: "There is already a registered phone as main"
+			});
+		}
+
 		return res.json(await this.create(req.body));
 	}
 
